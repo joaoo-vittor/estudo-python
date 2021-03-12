f7adc7655bc560e8213fb3ca7cb88edf8327fdc5
João Vitor <joaoo.vittor007@gmail.com>
2021-03-11 23:40:33 -0300

    Revisão de Python

---
+++ revisao/aula33Modulos/aula33Modulo.py
@@ -0,0 +1,8 @@
+def soma(x, y):
+  return x + y
+
+def div(x, y):
+  return x / y if y != 0 else 0
+
+def nomeModulo():
+  return __name__
--- revisao/aula35.py
+++ revisao/aula35.py
@@ -1,3 +1,18 @@
 """
 Aula 35
+
+Problema dos parâmetros mutáveis em funções
+
 """
+
+def lista_de_clientes(clientes_iteraveis, lista=None):
+  lista = [] if lista is None else None
+  lista.extend(clientes_iteraveis)
+  return lista
+
+clientes1 = lista_de_clientes(['joao', 'otavio', 'manu'])
+clientes2 = lista_de_clientes(['Jonas', 'Sofia', 'Maria'])
+clientes3 = lista_de_clientes(['Jose'])
+
+print(clientes1)
+print(clientes2)
