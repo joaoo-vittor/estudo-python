e008cc814e62b75e2fc4038d50b31198db38e743
João Vitor <joaoo.vittor007@gmail.com>
2021-04-03 01:52:47 -0300

    DESC: Estudo de biblioteca PyQt5

--- modulosPython/aula13.py
+++ modulosPython/aula13.py
@@ -1,3 +1,34 @@
 """
 Aula 13
+
+Deque - Trabalhando com LIFO e FIFO
+
 """
+
+from collections import deque
+
+"""
+fila = deque()
+
+fila.append('joao')
+fila.append('maria')
+fila.append('jose')
+fila.append('luiz')
+fila.append('marcos')
+
+print(f'Saiu: {fila.popleft()}')
+print(f'Saiu: {fila.popleft()}')
+print(f'Saiu: {fila.popleft()}')
+print(f'Saiu: {fila.popleft()}')
+
+for nome in fila:
+    print(nome)
+"""
+
+fila = deque(maxlen=5)
+
+fila.extend([1, 2, 3, 4])
+fila.append(5)
+fila.append(6)
+
+print(fila)
